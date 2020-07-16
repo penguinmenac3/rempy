19d5468326e13d061b63ecdd4589c5491639c755
Michael F <mail@michaelfuerst.de>
2020-07-16 22:43:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author_email='mail@michaelfuerst.de',
     entry_points={
         'console_scripts': [
-            'rempy = rempy',
+            'rempy = rempy:main',
         ]
     }
 )
